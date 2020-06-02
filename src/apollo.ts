07929a29ff70d48d1cf7948a25f0b9681a0cf38e
preciouStar <seongjin605@users.noreply.github.com>
2020-06-03 06:01:19 +0900

    Create apolllo clientState and mutation of resolver

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,38 @@
 import ApolloClient, { Operation } from 'apollo-boost';
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        _typename: 'Auth',
+        isLoggedIn: Boolean(localStorage.getItem('jwt'))
+      }
+    },
+    resolvers: {
+      Mutation: {
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem('jwt', token);
+          cache.writeDate({
+            data: {
+              __typename: 'Auth',
+              isLoggedIn: true
+            }
+          });
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem('jwt');
+          cache.writeDate({
+            data: {
+              __typename: 'Auth',
+              isLoggedIn: false
+            }
+          });
+          return null;
+        }
+      }
+    }
+  },
   request: async (operation: Operation) => {
     operation.setContext({
       headers: {
